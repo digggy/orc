d0f7223c361e5f08f3672e12a39e4f8bb0fa2f7c
Digdarshan <d.kunwar@jacobs-university.de>
2021-03-21 14:59:08 +0100

    a script for testing purpose

---
+++ local_request.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+# cd to the directory of the bash script and get the files in relative location
+cd "$parent_path"
+
+REQUEST_METHOD=GET
+CONTENT_TYPE=application/yang-data+json
+HTTP_ACCEPT=application/yang-data+json
+REQUEST_URI=/cgi-bin/restconf
+CONTENT_LENGTH=0
+
+echo "
+REQUEST_METHOD= $REQUEST_METHOD
+REQUEST_URI= $REQUEST_URI
+
+HTTP_ACCEPT= $HTTP_ACCEPT
+CONTENT_TYPE= $CONTENT_TYPE
+CONTENT_LENGTH= $CONTENT_LENGTH"
+
+# send request with the env variable set above
+REQUEST_METHOD=$REQUEST_METHOD \
+CONTENT_TYPE=$CONTENT_TYPE \
+HTTP_ACCEPT=$HTTP_ACCEPT \
+REQUEST_URI=$REQUEST_URI \
+CONTENT_LENGTH=$CONTENT_LENGTH \
+ ./cmake-build-debug-openwrt/restconf
