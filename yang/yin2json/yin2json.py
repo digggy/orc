9d14bb169df4e56909a8be8040081861ac7ef0c2
Digdarshan <d.kunwar@jacobs-university.de>
2021-04-01 23:52:51 +0200

    yang2json with groupings

--- yang/yin2json/yin2json.py
+++ yang/yin2json/yin2json.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import copy
 
 import xmltodict
 from mako.lookup import TemplateLookup
@@ -9,8 +10,8 @@ from mako.template import Template
 # Get the current directory for this file
 dirname = os.path.dirname(os.path.realpath(__file__))
 # Template Lookup
-mylookup = TemplateLookup(directories=[os.path.join(dirname, "template")])
-header_file = Template(filename=os.path.join(dirname, "template/yang.h.templ"), lookup=mylookup)
+mylookup = TemplateLookup(directories=[os.path.join(dirname, "./template")])
+header_file = Template(filename=os.path.join(dirname, "./template/yang.h.templ"), lookup=mylookup)
 
 types = {}
 ALLOWED_TYPES = {
@@ -25,7 +26,14 @@ ALLOWED_TYPES = {
     "uint64": "UINT_64",
     "binary": "BINARY",
     "boolean": "BOOLEAN",
-    "decimal64": "DECIMAL_64"
+    "decimal64": "DECIMAL_64",
+    "enumeration": "ENUMERATION",
+    "union": "UNION",
+    "bits": "BITS",
+    "yang:xpath1.0": "STRING",
+    "counter32": "INT_32",
+    "leafref": "LEAF_REF",
+    "empty": "EMPTY"
 }
 
 
@@ -57,7 +65,26 @@ class Imported:
     def get_types(self):
         return self.types
 
+class Grouping:
+    def __init__(self):
+        self.tree = {}
+        self.current_size = 0
 
+    def generate_name(self):
+        return "grouping{}".format(self.current_size)
+
+    def add_grouping(self, name, value={}):
+        if name in self.tree:
+            print("Duplicate grouping: {}\nPlease check YANG model".format(name))
+            return
+        self.tree[name] = value
+        self.current_size += 1
+
+    def find(self, name):
+        if name in self.tree:
+            return self.tree[name]["map"]
+        return None
+    
 def convert_yin_to_json(data):
     val = xmltodict.parse(data)
     return json.loads(json.dumps(val))
@@ -90,6 +117,8 @@ def extract_uci_statements(generated, key, value, imported):
         generated["section"] = value["@name"]
     if key == imported.openwrt_prefix + ":leaf-as-name":
         generated["leaf-as-name"] = value["@name"]
+    if key == imported.openwrt_prefix + ":leaf-as-type":
+        generated["leaf-as-type"] = value["@name"]
 
 
 def extract_type_statements(generated, key, value, imported):
@@ -111,13 +140,58 @@ def extract_type_statements(generated, key, value, imported):
             "pattern": "^" + value["pattern"]["@value"] + "$"
         }
     if range_allowed(type_name) and "range" in value:
-        range_split = value["range"]["@value"].split("..", 1)
+        range_split = get_range(value["range"]["@value"].split("..", 1), type_name)
         type_name = {
             "leaf-type": type_name,
             "from": range_split[0],
             "to": range_split[1]
         }
     generated["leaf-type"] = type_name
+
+def get_range(range_split, type_name):
+    if len(range_split) == 0:
+        return [0, 0]
+    try:
+        val1 = int(range_split[0])
+    except ValueError:
+        val1 = get_min_or_max(range_split[0], type_name)
+    try:
+        val2 = int(range_split[1])
+    except ValueError:
+        val2 = get_min_or_max(range_split[1], type_name)
+    return [val1, val2]
+
+def get_min_or_max(x, t):
+    if x == "max":
+        if t == "int8":
+            return (2**7)-1
+        elif t == "int16":
+            return (2**15)-1
+        elif t == "int32":
+            return (2**31)-1
+        elif t == "int64":
+            return (2**63)-1
+        elif t == "uint8":
+            return (2**8)-1
+        elif t == "uint16":
+            return (2**16)-1
+        elif t == "uint32":
+            return (2**32)-1
+        else:
+            return (2**64)-1
+    elif x == "min":
+        if t == "int8":
+            return -(2**7)
+        elif t == "int16":
+            return -(2**15)
+        elif t == "int32":
+            return -(2**31)
+        elif t == "int64":
+            return -(2**63)
+        else:
+            return 0
+    else:
+        return 0
 
 
 def handle_typedef(typedefs):
@@ -129,9 +203,15 @@ def handle_typedef(typedefs):
             "leaf-type": typedefs["type"]["@name"]
         }
         if converted["leaf-type"] == "string" and "pattern" in typedefs["type"]:
-            converted["pattern"] = "^" + typedefs["type"]["pattern"]["@value"] + "$"
+            if isinstance(typedefs["type"]["pattern"], list):
+                regex = ""
+                for val in typedefs["type"]["pattern"]:
+                    regex += "({})|".format(val["@value"])
+                converted["pattern"] = "^({})$".format(regex[:len(regex)-1])
+            else:
+                converted["pattern"] = "^" + typedefs["type"]["pattern"]["@value"] + "$"
         if range_allowed(converted["leaf-type"]) and "range" in typedefs["type"]:
-            range_split = typedefs["type"]["range"]["@value"].split("..", 1)
+            range_split = get_range(typedefs["type"]["range"]["@value"].split("..", 1), converted["leaf-type"])
             converted["from"] = range_split[0]
             converted["to"] = range_split[1]
         types[typedefs["@name"]] = converted
@@ -140,20 +220,50 @@ def handle_typedef(typedefs):
             handle_typedef(item)
 
 
-def process_node(generated, key, value, imported):
+def process_node(generated, key, value, imported, groupings):
     if isinstance(value, dict):
         inner_key = value["@name"]
         if "mandatory" in value and value["mandatory"]["@value"] == "true":
             if not ("mandatory" in generated):
                 generated["mandatory"] = []
             generated["mandatory"].append(inner_key)
-        generated["map"][inner_key] = convert(value, imported, key)
+        generated["map"][inner_key] = convert(value, imported, groupings, key)
     elif isinstance(value, list):
         for inner_value in value:
-            process_node(generated, key, inner_value, imported)
+            process_node(generated, key, inner_value, imported, groupings)
 
+def handle_grouping(value, imported, groupings):
+    if isinstance(value, dict):
+        grouping_name = ""
+        if "@name" in value:
+            grouping_name = value["@name"]
+        else:
+            grouping_name = groupings.generate_name()
+        groupings.add_grouping(grouping_name, convert(value, imported, groupings, "grouping"))
+    elif isinstance(value, list):
+        for val in value:
+            handle_grouping(val, imported, groupings)
 
-def convert(level, imported, object_type=None):
+def extract_uses(generated, value, imported, groupings):
+    if isinstance(value, list):
+        for val in value:
+            extract_uses(generated, val, imported, groupings)
+        return
+    if "@name" not in value:
+        print("Invalid use of 'uses': {}".format(value))
+        return
+    name = value["@name"]
+    grouping = copy.deepcopy(groupings.find(name))
+    if grouping == None:
+        print("Could not find {} in groupings".format(name))
+        return
+    for key, val in grouping.items():
+        if key in generated["map"].keys():
+            print("A node with name '{}' already exists".format(key))
+            continue
+        generated["map"][key] = val
+
+def convert(level, imported, groupings, object_type=None):
     generated = {}
     changed_level = level
     if len(changed_level) == 1 and "module" in changed_level:
@@ -168,6 +278,8 @@ def convert(level, imported, object_type=None):
     generated["map"] = {}
     for key, value in changed_level.items():
         extract_uci_statements(generated, key, value, imported)
+        if key == "grouping":
+            handle_grouping(value, imported, groupings)
         if key == "type":
             extract_type_statements(generated, key, value, imported)
         if key == "key":
@@ -179,7 +291,9 @@ def convert(level, imported, object_type=None):
             to_be_split = value["@value"]
             generated["unique"] = to_be_split.split()
         if key in ["container", "leaf", "leaf-list", "list"]:
-            process_node(generated, key, value, imported)
+            process_node(generated, key, value, imported, groupings)
+        if key == "uses":
+            extract_uses(generated, value, imported, groupings)
     return generated
 
 
@@ -209,11 +323,18 @@ def process_imported_types(args, imported):
                                 else:
                                     converted["pattern"].append("^" + item["type"]["pattern"]["@value"] + "$")
                             if range_allowed(converted["leaf-type"]) and "range" in item["type"]:
-                                range_split = item["type"]["range"]["@value"].split("..", 1)
+                                range_split = get_range(item["type"]["range"]["@value"].split("..", 1), converted["leaf-type"])
                                 converted["from"] = range_split[0]
                                 converted["to"] = range_split[1]
                             types[val["type_name"]] = converted
 
+def label_depths(js, parent=None, depth=0):
+    if js["type"] in ["module", "container", "list"]:
+        js["depth"] = depth
+        for key, val in js["map"].items():
+            label_depths(val, key, depth+1)
+            if parent is not None and val["type"] in ["container", "list"]:
+                val["parent"] = parent
 
 def main():
     parser = argparse.ArgumentParser(description='Preprocess YIN for OpenWrt RESTCONF')
@@ -226,14 +347,14 @@ def main():
 
     modules = []
 
-    # Input yin files
     for file in args.file:
         with open(file) as yin:
             data = yin.read()
-            # converting the yin data to json format
             js = convert_yin_to_json(data)
+            groupings = Grouping()
             imported = Imported()
-            js = convert(js, imported)
+            js = convert(js, imported, groupings)
+            label_depths(js)
             modules.append((os.path.basename(file).split('.')[0], js))
 
     process_imported_types(args, imported)
