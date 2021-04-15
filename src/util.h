e7c778375930b459b176ee32a8078e41b21afd6f
Digdarshan <d.kunwar@jacobs-university.de>
2021-04-15 20:19:43 +0200

    sub-string replace methods

--- src/util.h
+++ src/util.h
@@ -1,6 +1,9 @@
 #ifndef RESTCONF_UTIL_H
 #define RESTCONF_UTIL_H
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 #include "cgi.h"
 
 int split_pair_by_char(char *split, char **first, char **second,
@@ -9,5 +12,11 @@ char *str_dup(const char *c);
 char *strn_dup(const char *c, size_t to);
 int is_in_vector(char **vec, char *value);
 char *concat(const char *string_one, const char *string_two);
+
+char * appendstr(char * string, const char * append);
+char * strtokk(char * string, const char * strf);
+char * strrep(const char * string, const char * strf, const char * strr);
+
+
 //char *add_space(char* string);
 #endif  // RESTCONF_UTIL_H
