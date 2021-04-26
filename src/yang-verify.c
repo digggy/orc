523bf619851f0397c42a9a256afe764e0e07e37d
Digdarshan <d.kunwar@jacobs-university.de>
2021-04-26 23:54:48 +0200

    WIP working on tests

--- src/yang-verify.c
+++ src/yang-verify.c
@@ -160,6 +160,8 @@ static int regex_verify_value(const char* regex, const char* value) {
     return 1;
   }
   regfree(&regex_o);
+    printf("Regex %s \n", regex);
+    printf("Value %s \n", value);
   return 0;
 }
 
